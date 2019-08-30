fdd5e84af9b742f3ee309628f590b341e6aa6101
aoortwijn <aoortwijn@gmail.com>
2019-08-30 11:25:11 +0200

    eerste commit

---
+++ Ganzenbord.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Ganzenbord {
+	public static void main(String[] args){
+		
+		Random random = new Random(); 
+		int a = random.nextInt(6);
+		System.out.println(a);
+		
+		
+		
+		
+	}
+}
+
+
+

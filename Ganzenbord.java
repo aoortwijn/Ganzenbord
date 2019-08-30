821ede24a9aeb852add03936ddbcc15929a2a895
aoortwijn <aoortwijn@gmail.com>
2019-08-30 12:46:43 +0200

    toevoeging dobbelsteen

--- Ganzenbord.java
+++ Ganzenbord.java
@@ -3,15 +3,32 @@ import java.util.Random;
 public class Ganzenbord {
 	public static void main(String[] args){
 		
-		Random random = new Random(); 
-		int a = random.nextInt(6);
-		System.out.println(a);
 		
+		Dobbelsteen dobbelsteen1 = new Dobbelsteen();
+		dobbelsteen1.werpen();
 		
-		
+		Gans gans1 = new Gans();
+		gans1.positie(dobbelsteen1);
 		
 	}
 }
 
+class Dobbelsteen{
+	int uitkomst;
+	void werpen() {
+		Random willekeur = new Random ();
+		int uitkomst = willekeur.nextInt(6);
+		System.out.println(uitkomst + 1);
+	}
+}
+
+
+class Gans{
+	int positie;
+	void positie(Dobbelsteen dobbelsteen) {
+		int positie = 1;
+		System.out.println("Je staat op vakje " + positie);
+	}
+}
 
 

f9d61b1a6b4d3634370938b0be19f44cdbbbb531
aoortwijn <aoortwijn@gmail.com>
2019-08-31 19:34:58 +0200

    startniveau, moet alleen nog spel stoppen toevoegen

--- Ganzenbord.java
+++ Ganzenbord.java
@@ -26,7 +26,7 @@ class Bord{
 	}
 	void zetten (Gans[] gans1) {
 		Scanner scanner = new Scanner(System.in);
-		for(int x = 0; x < 10 ; x++) {
+		for(int x = 0; x < 30 ; x++) {
 			for(int y = 0 ; y < gans1.length ; y++) {
 			dobbelsteen1.werpen();	
 			gans1[y].lopen(dobbelsteen1);
@@ -62,14 +62,20 @@ class Gans{
 	void lopen(Dobbelsteen dobbelsteen) {
 		positie = positie + dobbelsteen.uitkomst;
 		System.out.println(kleur+": Je staat op vakje: " + positie);
-		if(positie == 3) {
+		if(positie == 25 | positie == 45) {
 			System.out.println("Ga terug naar start");
 			positie = 1;
-		}else if (positie == 5) {
+		}else if (positie == 10 | positie == 20 | positie == 30 | positie == 40 | positie == 50) {
+// Kan de regel hierboven ook korter?			
 			int positie2 = positie + dobbelsteen.uitkomst;
-			System.out.println("Bonusstapjes! Je staat op vakje: " + positie2); 
-			positie = positie2;
-			
+			System.out.println("Bonusstapjes! Je gaat naar vakje: " + positie2); 
+			positie = positie2;	
+		}else if(positie == 23) {
+			System.out.println("Je zit in de gevangenis. Het spel is over");
+// spel stoppen, hoe? 			
+		}else if(positie >= 63 ) {
+			System.out.println(kleur + "heeft het spel gewonnen");
+// spel stoppen, hoe?			
 		}
 	}
 }
